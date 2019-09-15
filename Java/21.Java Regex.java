9b50ff46ef4bdda9fb48f23afa23063b6f104eca
Rayrone Zirtany <rayronezirtany@gmail.com>
2019-09-14 21:07:17 -0300

    Dias 16, 17 e 18; Regex

---
+++ Java/21.Java Regex.java
@@ -0,0 +1,25 @@
+/*
+	Java Regex
+	URL: https://www.hackerrank.com/challenges/java-regex/problem
+*/
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.Scanner;
+
+class Solution{
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        while(in.hasNext()){
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+
+    }
+}
+
+class MyRegex {
+
+    public String pattern = "^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"; 
+
+}
